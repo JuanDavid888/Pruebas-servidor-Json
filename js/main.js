7054919be34ece2aff7d016a5d5f4712b2fe13f8
JuanDavid888 <190215642+JuanDavid888@users.noreply.github.com>
2025-03-27 15:13:57 -0500

    feat: :alien: Se añadió la función de editar usuarios y se modificó la estructura de las funciones

--- js/main.js
+++ js/main.js
@@ -74,57 +74,101 @@
 //     .catch(error => alert(error))
 // }
 
-// const findUsers = async () => {
-//     const config = {
-//         method: "GET", // Obtener
-//     };
-//     const response = await fetch(`http://localhost:5600/users`, config);
-//     const result = await response.json();
-//     return result;
-// }
-// let seeUsers = confirm("¿Deseas visualizar todos los usuarios?");
-// if(seeUsers){
-//     findUsers()
-//     .then(result => console.table(result))
-//     .catch(error => alert(error))
-// }
+const findUsers = async () => {
+    const config = {
+        method: "GET", // Obtener
+    };
+    const response = await fetch(`http://localhost:5600/users`, config);
+    const result = await response.json();
+    return result;
+}
+const viewUsers = async () => {
+    let seeUsers = confirm("¿Deseas visualizar todos los usuarios?");
+    if(seeUsers){
+        findUsers()
+        .then(result => console.table(result))
+        .catch(error => alert(error))
+    }
+}
 
-// const saveUser = async(data) => {
-//     const config = {
-//         method: "POST", // Guardar
-//         body: JSON.stringify(data)
-//     }
-//     const response = await fetch("http://localhost:5600/users", config);
-//     const result = await response.json();
-//     return result;
-// }
-// while (confirm("¿Desea insertar un usuario?")) {
-//     const data = {};
+const saveUser = async(data) => {
+    const config = {
+        method: "POST", // Guardar
+        body: JSON.stringify(data)
+    }
+    const response = await fetch("http://localhost:5600/users", config);
+    const result = await response.json();
+    return result;
+}
+const formularyAdd = async () => {
+    while (confirm("¿Desea insertar un usuario?")){
+        const users = await findUsers(); // Obtiene datos de los usuarios guardados
+        const userIds = users.map(user => user.id); // Recorre  usuarios y verifica ids
+        const data = {};
+    
+        while (true) {
+            data.id = (prompt("Ingrese el ID"));
+            if (data.id !== "" && !userIds.includes(data.id)) break;
+            alert("El ID debe no existir");
+        }
+    
+        while (true){
+            data.name = prompt("Ingrese el nombre").trim().toLowerCase().split(" ").map(palabra => palabra.charAt(0).toUpperCase() + palabra.slice(1)).join(" ");
+    
+            if (data.name !== "" && isNaN(data.name) && !/^[0-9]+$/.test(data.name)) break;
+            alert("El nombre ingresado no es válido.");
+        }
+    
+        while (true){
+            data.last = prompt("Ingrese el apellido").trim().toLowerCase().split(" ").map(palabra => palabra.charAt(0).toUpperCase() + palabra.slice(1)).join(" ");
+    
+            if (data.last !== "" && isNaN(data.last) && !/^[0-9]+$/.test(data.last)) break;
+            alert("El apellido ingresado no es válido.");
+        } 
+    
+        saveUser(data)
+        .then(result => alert(JSON.stringify(result)))
+        .catch(error => alert(error))
+    }
+}
 
-//     while (true) {
-//         data.id = (prompt("Ingrese el ID"));
-//         if (data.id !== "") break;
-//         alert("El ID debe contener un caracter");
-//     }
-
-//     while (true) {
-//         data.name = prompt("Ingrese el nombre").trim().toLowerCase().split(" ").map(palabra => palabra.charAt(0).toUpperCase() + palabra.slice(1)).join(" ");
-
-//         if (data.name !== "" && isNaN(data.name) && !/^[0-9]+$/.test(data.name)) break;
-//         alert("El nombre ingresado no es válido.");
-//     }
-
-//     while (true) {
-//         data.last = prompt("Ingrese el apellido").trim().toLowerCase().split(" ").map(palabra => palabra.charAt(0).toUpperCase() + palabra.slice(1)).join(" ");
-
-//         if (data.last !== "" && isNaN(data.last) && !/^[0-9]+$/.test(data.last)) break;
-//         alert("El apellido ingresado no es válido.");
-//     } 
-
-//     saveUser(data)
-//     .then(result => alert(JSON.stringify(result)))
-//     .catch(error => alert(error))
-// }
+const editUser = async(data) => {
+    const {id, ...dataUpdate} = data
+    const config = {
+        method: "PATCH", // Actualizar
+        body: JSON.stringify(dataUpdate)
+    }
+    const response = await fetch(`http://localhost:5600/users/${id}`, config);
+    const result = await response.json();
+    return result;
+}
+const formularyEdit = async () => {
+    while(confirm("¿Desea actualizar un usuario?")){
+        const users = await findUsers(); // Obtiene datos de los usuarios guardados
+        const userIds = users.map(user => user.id); // Recorre  usuarios y verifica ids
+        const data = {}
+        
+        while (true) {
+            data.id = (prompt("Ingrese el ID"));
+            if (data.id !== "" && userIds.includes(data.id)) break;
+            alert("El ID debe existir");
+        }
+        while (true) {
+            data.name = confirm("¿Desea actualizar el nombre?")? prompt("Ingrese el nuevo nombre").trim().toLowerCase().split(" ").map(palabra => palabra.charAt(0).toUpperCase() + palabra.slice(1)).join(" ") : undefined
+            if (data.name !== "" && isNaN(data.name) && !/^[0-9]+$/.test(data.name)) break;
+            alert("El nombre ingresado no es válido.");
+        }
+        while (true) {
+            data.last = confirm("¿Desea actualizar el apellido?")? prompt("Ingrese el nuevo apellido").trim().toLowerCase().split(" ").map(palabra => palabra.charAt(0).toUpperCase() + palabra.slice(1)).join(" ") : undefined
+            if (data.last !== "" && isNaN(data.last) && !/^[0-9]+$/.test(data.last)) break;
+            alert("El apellido ingresado no es válido.");
+        }
+    
+        editUser(data)
+        .then(result => alert(JSON.stringify(result)))
+        .catch(error => alert(error))
+    }
+}
 
 // Fetch = traer
 // Get = Obtener
