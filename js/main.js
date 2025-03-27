9b21cd2ec6178286f08ac41b78d8aa4bf196ec49
JuanDavid888 <190215642+JuanDavid888@users.noreply.github.com>
2025-03-27 15:53:34 -0500

    feat: :art: Se mejoró la estructura del código y se metieron validaciones

--- js/main.js
+++ js/main.js
@@ -1,78 +1,134 @@
-// const saveProduct = async(data) => {
-//     const config = {
-//         method: "POST", // Guardar
-//         body: JSON.stringify(data)
-//     }
-//     const response = await fetch("http://localhost:5600/products", config);
-//     const result = await response.json();
-//     return result;
-// }
-// while(confirm("¿Desea insertar un dato?")){
-//     const data = {
-//     id: prompt("Ingrese el id", "Mic330"),
-//     name: prompt("Ingrese el nombre", "Mac mini"),
-//     price: Number(prompt("Ingrese el precio", 600)),
-//     category: prompt("Ingrese la categoría", "Technology")
-//     }
-//     saveProduct(data)
-//     .then(result => alert(JSON.stringify(result)))
-//     .catch(error => alert(error))
-// }
+// Funciones para los productos
 
-// const editProduct = async(data) => {
-//     const {id, ...dataUpdate} = data
-//     const config = {
-//         method: "PATCH", // Actualizar
-//         body: JSON.stringify(dataUpdate)
-//     }
-//     const response = await fetch(`http://localhost:5600/products/${id}`, config);
-//     const result = await response.json();
-//     return result;
-// }
-// while(confirm("¿Desea actualizar un producto?")){
-//     const data = {}
+const findProduct = async() => {
+    const config = {
+        method: "GET", // Obtener
+    }
+    const response = await fetch(`http://localhost:5600/products`, config);
+    const result = await response.json();
+    return result;
+}
+const viewProducts = async () => {
+    let seeProducts = confirm("¿Deseas visualizar todos los productos?");
+    if(seeProducts){
+        findProduct()
+        .then(result => console.table(result))
+        .catch(error => alert(error))
+    }
+}
 
-//     data.id = prompt("Ingrese el id del producto", "Mic330");
-//     data.name = (confirm("¿Desea actualizar el nombre del producto?"))? prompt("Ingrese el nuevo nombre") : undefined
-//     data.price = (confirm("¿Desea actualizar el precio del producto?"))? Number(prompt("Ingrese el nuevo precio")) : undefined
-//     data.category = (confirm("¿Desea actualizar la categoría del producto?"))? prompt("Ingrese la nueva categoría") : undefined
+const saveProduct = async(data) => {
+    const config = {
+        method: "POST", // Guardar
+        body: JSON.stringify(data)
+    }
+    const response = await fetch("http://localhost:5600/products", config);
+    const result = await response.json();
+    return result;
+}
+const formularyAddProduct = async () => {
+    const products = await findProduct(); // Obtiene datos de los productos guardados
+    const productIds = products.map(product => product.id); // Recorre  productos y verifica ids
+    const data = {};
 
-//     editProduct(data)
-//     .then(result => alert(JSON.stringify(result)))
-//     .catch(error => alert(error))
-// }
+    while(confirm("¿Desea insertar un producto?")){
+        while (true) {
+            data.id = (prompt("Ingrese el ID"));
+            if (data.id !== "" && !productIds.includes(data.id)) break;
+            alert("El ID debe no existir");
+        }
+        while (true){
+            data.name = prompt("Ingrese el nombre").trim().toLowerCase().split(" ").map(palabra => palabra.charAt(0).toUpperCase() + palabra.slice(1)).join(" ");
+            if (data.name !== "" && isNaN(data.name) && !/^[0-9]+$/.test(data.name)) break;
+            alert("El nombre ingresado no es válido.");
+        }
+        while (true){
+            data.price = Number(prompt("Ingrese el precio"));
+            if (data.price !== "" && !isNaN(data.price)) break;
+            alert("El precio ingresado no es válido.");
+        }
+        while (true){
+            data.category = prompt("Ingrese la categoría").trim().toLowerCase().split(" ").map(palabra => palabra.charAt(0).toUpperCase() + palabra.slice(1)).join(" ");
+            if (data.category !== "" && isNaN(data.category) && !/^[0-9]+$/.test(data.category)) break;
+            alert("La categoría ingresada no es válida.");
+        }
+        saveProduct(data)
+        .then(result => alert(JSON.stringify(result)))
+        .catch(error => alert(error))
+    } 
+}
 
-// const removeProduct = async(data) => {
-//     const {id} = data
-//     const config = {
-//         method: "DELETE", // Eliminar
-//     }
-//     const response = await fetch(`http://localhost:5600/products/${id}`, config);
-//     const result = await response.json();
-//     return result;
-// }
-// while(confirm("¿Desea eliminar un producto?")){
-//     const data = {}
-//     data.id = prompt("Ingrese el id del producto", "Mic330")
+const editProduct = async(data) => {
+    const {id, ...dataUpdate} = data
+    const config = {
+        method: "PATCH", // Actualizar
+        body: JSON.stringify(dataUpdate)
+    }
+    const response = await fetch(`http://localhost:5600/products/${id}`, config);
+    const result = await response.json();
+    return result;
+}
+const formularyEditProduct = async () => {
+    while(confirm("¿Desea actualizar un producto?")){
+        const products = await findProduct(); // Obtiene datos de los productos guardados
+        const productIds = products.map(product => product.id); // Recorre  productos y verifica ids
+        const data = {}
+    
+        while (true) {
+            data.id = (prompt("Ingrese el ID"));
+            if (data.id !== "" && productIds.includes(data.id)) break;
+            alert("El ID debe existir");
+        }
+        while (true) {
+            data.name = confirm("¿Desea actualizar el nombre?")? prompt("Ingrese el nuevo nombre").trim().toLowerCase().split(" ").map(palabra => palabra.charAt(0).toUpperCase() + palabra.slice(1)).join(" ") : undefined
+            if (data.name !== "" && isNaN(data.name) && !/^[0-9]+$/.test(data.name)) break;
+            alert("El nombre ingresado no es válido.");
+        }
+        while (true) {
+            data.price = confirm("¿Desea actualizar el precio?")? Number(prompt("Ingrese el nuevo precio")) : undefined
+            if (data.price !== "" && !isNaN(data.price)) break;
+            alert("El precio ingresado no es válido.");
+        }
+        while (true) {
+            data.category = confirm("¿Desea actualizar la categoría?")? prompt("Ingrese la nueva categoría").trim().toLowerCase().split(" ").map(palabra => palabra.charAt(0).toUpperCase() + palabra.slice(1)).join(" ") : undefined
+            if (data.category !== "" && isNaN(data.category) && !/^[0-9]+$/.test(data.category)) break;
+            alert("La categoría ingresada no es válida.");
+        }
+    
+        editProduct(data)
+        .then(result => alert(JSON.stringify(result)))
+        .catch(error => alert(error))
+    }
+}
 
-//     removeProduct(data)
-//     .then(result => alert(JSON.stringify(result)))
-//     .catch(error => alert(error))
-// }
+const removeProduct = async(data) => {
+    const {id} = data
+    const config = {
+        method: "DELETE", // Eliminar
+    }
+    const response = await fetch(`http://localhost:5600/products/${id}`, config);
+    const result = await response.json();
+    return result;
+}
+const formularyRemoveProduct = async () => {
+    const products = await findProduct(); // Obtiene datos de los productos guardados
+    const productIds = products.map(product => product.id); // Recorre  productos y verifica ids
+    const data = {}
 
-// const findProduct = async() => {
-//     const config = {
-//         method: "GET", // Obtener
-//     }
-//     const response = await fetch(`http://localhost:5600/products`, config);
-//     const result = await response.json();
-//     return result;
-// }
-// confirm("¿Deseas visualizar todos los productos?");{
-//     findProduct()
-//     .then(result => console.table(result))
-//     .catch(error => alert(error))
-// }
+    while(true){
+        let deleteProduct = confirm("¿Desea eliminar un producto?");
+        if(deleteProduct){
+            data.id = prompt("Ingrese el id del producto").trim();
+            if (data.id !== "" && productIds.includes(data.id)) break;
+            alert("El ID ingresado no existe")}
+    }
+    
+    removeProduct(data)
+    .then(result => alert(JSON.stringify(result)))
+    .catch(error => alert(error))
+}
+
+// Funciones para los usuarios
 
 const findUsers = async () => {
     const config = {
@@ -100,7 +156,7 @@ const saveUser = async(data) => {
     const result = await response.json();
     return result;
 }
-const formularyAdd = async () => {
+const formularyAddUser = async () => {
     while (confirm("¿Desea insertar un usuario?")){
         const users = await findUsers(); // Obtiene datos de los usuarios guardados
         const userIds = users.map(user => user.id); // Recorre  usuarios y verifica ids
@@ -142,7 +198,7 @@ const editUser = async(data) => {
     const result = await response.json();
     return result;
 }
-const formularyEdit = async () => {
+const formularyEditUser = async () => {
     while(confirm("¿Desea actualizar un usuario?")){
         const users = await findUsers(); // Obtiene datos de los usuarios guardados
         const userIds = users.map(user => user.id); // Recorre  usuarios y verifica ids
@@ -179,17 +235,19 @@ const removeUser = async(data) => {
     const result = await response.json();
     return result;
 }
-const formularyRemove = async () => {
+const formularyRemoveUser = async () => {
     const users = await findUsers(); // Obtiene datos de los usuarios guardados
     const userIds = users.map(user => user.id); // Recorre  usuarios y verifica ids
     const data = {}
 
-    while(confirm("¿Desea eliminar un usuario?")){
-        data.id = prompt("Ingrese el id del usuario").trim();
-        if (data.id !== "" && userIds.includes(data.id)) break;
-        alert("El ID ingresado no existe");
+    while(true){
+        let deleteUser = confirm("¿Desea eliminar un usuario?");
+        if(deleteUser){
+            data.id = prompt("Ingrese el id del usuario").trim();
+            if (data.id !== "" && userIds.includes(data.id)) break;
+            alert("El ID ingresado no existe")}
     }
-
+    
     removeUser(data)
     .then(result => alert(JSON.stringify(result)))
     .catch(error => alert(error))
