3fa55af18ff6e1ed1ff6fa31b0a80be5a4030b69
JuanDavid888 <190215642+JuanDavid888@users.noreply.github.com>
2025-03-29 11:30:07 -0500

    feat: :alien: Se agregaron validaciones a las funciones de usuarios
    Se validaron los datos de usuarios y se arreglaron algunas validaciones de productos

--- js/main.js
+++ js/main.js
@@ -27,7 +27,8 @@ const viewProducts = async () => {
 }
 
 // Ver producto por id
-const FindProductById = async(id) => {
+const FindProductById = async(data) => {
+    const {id} = data
     const url = new URL(`https://67e686886530dbd311105634.mockapi.io/products/${id}`)
     const config = {
         method: "GET", // Obtener los datos
@@ -39,16 +40,17 @@ const FindProductById = async(id) => {
 const viewProductById = async () => {
     const products = await findProduct()
     const ids = products.map(product => product.id)
+    const data = {}
 
     let seeProduct = confirm("¿Deseas visualizar un producto por su id?");
     if(seeProduct){
         while(true){
-            const id = prompt("Ingrese el id del producto")
-            if (id !== "" && !isNaN(id) && ids.includes(id)) break;
+            data.id = prompt("Ingrese el id del producto")
+            if (data.id !== "" && !isNaN(data.id) && ids.includes(data.id)) break;
             alert("El ID ingresado no existe")
         }
     }
-    FindProductById(id)
+    FindProductById(data)
         .then(result => console.table(result))
         .catch(error => alert(error))
 }
@@ -173,16 +175,19 @@ const formularyRemoveProduct = async () => {
         let deleteProduct = confirm("¿Desea eliminar un producto?");
         if (!deleteProduct) {
             alert("Acción cancelada");
+            return;
+        }
+        let id = prompt("Ingrese el id del usuario");
+        if (id !== "" && !isNaN(id) && ids.includes(id)) {
+            data.id = id;
             break;
         }
-        data.id = prompt("Ingrese el id del producto");
-        if (data.id !== "" && !isNaN(data.id) && ids.includes(data.id)) break;
         alert("El ID ingresado no existe");
     }
 
     removeProduct(data)
-        .then((result) => alert(JSON.stringify(result)))
-        .catch((error) => alert(error));
+    .then((result) => alert(JSON.stringify(result)))
+    .catch((error) => alert(error));
 };
 
 // Usuarios
@@ -212,7 +217,8 @@ const viewUsers = async () => {
 }
 
 // Ver usuario por id
-const FindUserById = async(id) => {
+const FindUserById = async(data) => {
+    const {id} = data
     const url = new URL(`https://67e686886530dbd311105634.mockapi.io/users/${id}`)
     const config = {
         method: "GET", // Obtener los datos
@@ -222,10 +228,18 @@ const FindUserById = async(id) => {
     return result;
 }
 const viewUserById = async () => {
+    const users = await findUser()
+    const ids = users.map(user => user.id)
+    const data = {}
+
     let seeUser = confirm("¿Deseas visualizar un usuario por su id?");
     if(seeUser){
-        const id = prompt("Ingrese el id del usuario")
-        FindUserById(id)
+        while(true){
+            data.id = prompt("Ingrese el id del usuario")
+            if (data.id !== "" && !isNaN(data.id) && ids.includes(data.id)) break;
+            alert("El ID ingresado no existe")
+        }
+        FindUserById(data)
         .then(result => console.table(result))
         .catch(error => alert(error))
     }
@@ -251,10 +265,18 @@ const saveUser = async (data) => {
     return result;
 }
 const formularyAddUser = async () => {
+    const data = {}
+
     while (confirm("¿Desea insertar un usuario?")){
-        const data = {
-            name: prompt("Ingrese el nombre"),
-            last: prompt("Ingrese el apellido")
+        while(true){
+            data.name = prompt("Ingrese el nombre").trim().toLowerCase().split(" ").map(palabra => palabra.charAt(0).toUpperCase() + palabra.slice(1)).join(" ");
+            if (data.name !== "" && !/^[0-9]+$/.test(data.name)) break;
+            alert("El nombre ingresado es inválido, intente otra vez")
+        }
+        while(true){
+            data.last = prompt("Ingrese el apellido").trim().toLowerCase().split(" ").map(palabra => palabra.charAt(0).toUpperCase() + palabra.slice(1)).join(" ");
+            if (data.last !== "" && !/^[0-9]+$/.test(data.last)) break;
+            alert("El apellido ingresado es inválido, intente otra vez")
         }
 
     saveUser(data)
@@ -280,27 +302,38 @@ const editUser = async(data) => {
     return result;
 }
 const formularyEditUser = async () => {
+    const users = await findUser()
+    const ids = users.map(user => user.id)
+    const data = {}
+
     while(confirm("¿Desea actualizar un usuario?")){
-        const data = {}
-
-        data.id = prompt("Ingrese el id del usuario")
-        data.name = confirm("¿Desea actualizar el nombre?")? prompt("Ingrese el nuevo nombre") : undefined
-        data.last = confirm("¿Desea actualizar el apellido?")? prompt("Ingrese el nuevo apellido") : undefined
-
-        if (data.name || data.last) {
-            editUser(data)
-                .then(result => alert(JSON.stringify(result)))
-                .catch(error => alert(error));
-        } else {
-            alert("No se realizaron cambios.");
+        
+        while(true){
+            data.id = prompt("Ingrese el id del usuario")
+            if (data.id !== "" && !isNaN(data.id) && ids.includes(data.id)) break;
+            alert("El ID debe existir");
+        }
+        while (true) {
+            data.name = confirm("¿Desea actualizar el nombre?")? prompt("Ingrese el nuevo nombre").trim().toLowerCase().split(" ").map(palabra => palabra.charAt(0).toUpperCase() + palabra.slice(1)).join(" ") : undefined
+            if (data.name !== "" && isNaN(data.name) && !/^[0-9]+$/.test(data.name)) break;
+            alert("El nombre ingresado no es válido.");
+        }
+        while (true) {
+            data.last = confirm("¿Desea actualizar el apellido?")? prompt("Ingrese el nuevo apellido").trim().toLowerCase().split(" ").map(palabra => palabra.charAt(0).toUpperCase() + palabra.slice(1)).join(" ") : undefined
+            if (data.last !== "" && isNaN(data.last) && !/^[0-9]+$/.test(data.last)) break;
+            alert("El apellido ingresado no es válido.");
         }
     }
 
+    editUser(data)
+    .then(result => alert(JSON.stringify(result)))
+    .catch(error => alert(error))
 }
 
 // Eliminar usuario
 const removeUser = async(data) => {
-    const url = new URL(`https://67e686886530dbd311105634.mockapi.io/users/${data.id}`)
+    const {id} = data
+    const url = new URL(`https://67e686886530dbd311105634.mockapi.io/users/${id}`)
     const header = new Headers();
     header.append("Content-Type", "application/json");
 
@@ -313,17 +346,19 @@ const removeUser = async(data) => {
     return result;
 }
 const formularyRemoveUser = async () => {
+    const users = await findUser();
+    const ids = users.map((user) => user.id);
     const data = {}
 
     while(true){
         let deleteUser = confirm("¿Desea eliminar un usuario?");
-        if(deleteUser){
-            data.id = prompt("Ingrese el id del usuario").trim();
-            if (data.id !== "") break;
-            alert("El ID ingresado no existe")}
-        else{
-            alert("Acción cancelada")
+        if(!deleteUser){
+            alert("Acción cancelada");
+            return;
         }
+        data.id = prompt("Ingrese el id del usuario");
+        if (data.id !== "" && !isNaN(data.id) && ids.includes(data.id)) break;
+        alert("El ID ingresado no existe");
     }
     
     removeUser(data)
